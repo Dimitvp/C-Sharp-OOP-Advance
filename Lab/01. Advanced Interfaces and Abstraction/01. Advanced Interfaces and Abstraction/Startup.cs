b7b333a7a06c5841962c6358e5ff0186a8d20a6e
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2017-07-20 16:37:50 +0300

    1st lecture and first Lab

--- Lab/01. Advanced Interfaces and Abstraction/01. Advanced Interfaces and Abstraction/Startup.cs
+++ Lab/01. Advanced Interfaces and Abstraction/01. Advanced Interfaces and Abstraction/Startup.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _01.Advanced_Interfaces_and_Abstraction
+class StartUp
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        var radius = int.Parse(Console.ReadLine());
+        IDrawable circle = new Circle(radius);
+
+        var width = int.Parse(Console.ReadLine());
+        var height = int.Parse(Console.ReadLine());
+        IDrawable rect = new Rectangle(width, height);
+
+        circle.Draw();
+        rect.Draw();
+
     }
 }
